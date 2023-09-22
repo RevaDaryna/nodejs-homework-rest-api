09e0b5d62d57512ace5fdb348869d7339bbf7a88
RevaDaryna <revadara32@gmail.com>
2023-09-22 11:37:27 +0300

    hw2

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,20 +1,15 @@
 const Joi = require('joi');
 
-// const addSchema = Joi.object({
-//     name: Joi.string().required().messages({
-//       "any.required": "missing required name field",
-//       }),
-//     email: Joi.string().required().messages({
-//       "any.required": "missing required email field",
-//       }),
-//     phone: Joi.string().required().messages({
-//       "any.required": "missing required phone field",
-//       }),
-//   })
 const addSchema = Joi.object({
-    name: Joi.string(),
-    email: Joi.string(),
-    phone: Joi.string(),
+    name: Joi.string().required().messages({
+      "any.required": "missing required name field",
+      }),
+    email: Joi.string().required().messages({
+      "any.required": "missing required email field",
+      }),
+    phone: Joi.string().required().messages({
+      "any.required": "missing required phone field",
+      }),
   })
 
 module.exports = {addSchema}  
