fc775d18ab1b326fbd3b32f06b89330a9750a681
RevaDaryna <revadara32@gmail.com>
2023-09-27 02:32:49 +0300

    add changed

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,12 +1,12 @@
 const { isValidObjectId } = require("mongoose")
 const HttpError = require('../helpers/HttpError')
 
-const validId = (req, res, next) => {
-    const {id} = req.params;
-    if(isValidObjectId(id)){
-        next(HttpError(400, `${id} is not valid id`))
+const isvalidId = (req, res, next) => {
+    const {contactId} = req.params;
+    if(!isValidObjectId(contactId)){
+        next(HttpError(400, `${contactId} is not valid id`))
     }
     next()
 }
 
-module.exports = validId
+module.exports = isvalidId
